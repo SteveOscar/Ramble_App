845d37bf31512a32a4819ef8f311291125da32d4
Steven Olson <steveoscaro@gmail.com>
2016-06-11 09:55:17 -0600

    clean up helper text

--- SearchPage.js
+++ SearchPage.js
@@ -23,12 +23,12 @@ const styles = require('./Styles');
 
 function urlForExpensesQuery(country) {
   var querystring = country;
-  return 'http://localhost:3000/api/v1/expenses/' + querystring;
+  return 'http://www.ramblemap.com/api/v1/expenses/' + querystring;
 }
 
 function urlForTrendsQuery(country) {
   var querystring = country;
-  return 'http://localhost:3000/api/v1/trends/' + querystring;
+  return 'http://www.ramblemap.com/api/v1/trends/' + querystring;
 }
 
 function getTitle(Component) {
@@ -80,7 +80,7 @@ class SearchPage extends Component {
                     navigator: this.props.navigator}
       });
     } else {
-      this.setState({message: 'Location not recognized; please try again.'});
+      this.setState({message: 'Data for this location is not currently available, please try again.'});
     }
   }
 
@@ -103,35 +103,35 @@ class SearchPage extends Component {
     var spinner = this.state.isLoading ? (<ActivityIndicatorIOS size='large' style = {styles.spinner}/>) : (<View/>);
     return (
       <ScrollView>
-      <View style = {styles.container}>
-      <Image source={require('./Resources/Title.png')} style={styles.title}/>
-        <Text style={styles.description}>
-          Explore relative expenses and currency trends
-        </Text>
+        <View style = {styles.container}>
+        <Image source={require('./Resources/Title.png')} style={styles.title}/>
+          <Text style={styles.description}>
+            Explore relative expenses and currency trends. Choose your country:
+          </Text>
 
-        <FMPicker onUpdate={this.onUpdate.bind(this)}/>
-        <Text style={styles.helperText}>
-          View your currency's relative power in other countries:
-        </Text>
-        <TouchableHighlight onPress={this.onExpensesPressed.bind(this)}
-                            style={styles.button}
-                            underlayColor='#99d9f4'>
-          <Text style={styles.buttonText}>Relative Currency Power</Text>
-        </TouchableHighlight>
+          <FMPicker onUpdate={this.onUpdate.bind(this)}/>
+          <Text style={styles.helperText}>
+            Choose which dataset to view:
+          </Text>
+          <TouchableHighlight onPress={this.onExpensesPressed.bind(this)}
+                              style={styles.button}
+                              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Relative Currency Power</Text>
+          </TouchableHighlight>
 
-        <TouchableHighlight onPress={this.onTrendsPressed.bind(this)}
-                            style={styles.button}
-                            underlayColor='#99d9f4'>
-          <Text style={styles.buttonText}>Exchange Rate Trends</Text>
-        </TouchableHighlight>
-          {spinner}
-        <Image source={require('./Resources/glass.png')} style={styles.image}/>
-        <Text style={styles.description}>Powered By </Text>
-        <Text style={styles.link}
-              onPress={() => Linking.openURL('http://www.ramblemap.com')}>
-          RambleMap
-        </Text>
-      </View>
+          <TouchableHighlight onPress={this.onTrendsPressed.bind(this)}
+                              style={styles.button}
+                              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Exchange Rate Trends</Text>
+          </TouchableHighlight>
+            {spinner}
+          <Image source={require('./Resources/glass.png')} style={styles.image}/>
+          <Text style={styles.description}>Powered By </Text>
+          <Text style={styles.link}
+                onPress={() => Linking.openURL('http://www.ramblemap.com')}>
+            RambleMap
+          </Text>
+        </View>
       </ScrollView>
     );
   }
